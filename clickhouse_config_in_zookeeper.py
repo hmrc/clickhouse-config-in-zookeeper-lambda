3079f0f6803c7e57dfb0b5fc27291fc53f616fc4
Rhys Sharrem <rhys.sharrem@gmail.com>
2018-11-21 16:37:48 +0000

    WIP

--- clickhouse_config_in_zookeeper.py
+++ clickhouse_config_in_zookeeper.py
@@ -1,8 +1,45 @@
 import logging
+import boto3
+from kazoo.client import KazooClient
 
 logger = logging.getLogger('sensu_pagerduty_heartbeat')
 logger.setLevel(logging.INFO)
 
-
 def lambda_handler(event, context):
+    # Get zookeeper IPs from ec2
+    # Get clickhouse-server IPs and shards from ec2
+    # Write remote server config to Zookeeper
+    # Write graphite-rollup config to Zookeeper
+
+    ec2 = get_ec2_client()
+    response = ec2.describe_instances(Filters=[
+        {
+            "Name": "tag-key",
+            "Values": [
+                "clickhouse-server"
+            ]
+        }
+    ])
+
     return 'Hello World'
+
+def get_clickhouse_cluster_definition(ec2_client):
+    return ""
+
+
+def get_zookeeper_client(ec2_client):
+    response = ec2_client.describe_network_interfaces(Filters=[
+        {
+            "Name": "tag:Component",
+            "Values": [
+                "telemetry-zookeeper"
+            ]
+        }
+    ])
+    ips = [ i['PrivateIpAddress'] for i in response['NetworkInterfaces']]
+    zk = KazooClient(hosts=ips)
+    zk.start()
+    return zk
+
+def get_ec2_client():
+    return boto3.client('ec2')
--- tests/clickhouse_config_in_zookeeper_tests.py
+++ tests/clickhouse_config_in_zookeeper_tests.py
@@ -1,6 +1,83 @@
-from clickhouse_config_in_zookeeper import lambda_handler
+from clickhouse_config_in_zookeeper import lambda_handler, get_zookeeper_client, get_ec2_client
+from unittest.mock import patch, MagicMock
 import unittest
 
-class TestSensuPagerdutyHeartbeat(unittest.TestCase):
-    def test_hello_world_returned(self):
-        self.assertEquals(lambda_handler({}, {}), 'Hello World')
+class GetZookeeperClient(unittest.TestCase):
+
+    @patch('clickhouse_config_in_zookeeper.KazooClient')
+    def test_gets_network_interfaces_for_telemetry_zookeeper_and_creates_kazoo_client_with_ips(self, mock_kazoo_constructor):
+        mock_kazzoo_client = MagicMock()
+        mock_kazoo_constructor.return_value = mock_kazzoo_client
+        ec2_client = MagicMock()
+        sample_response = {
+            'NetworkInterfaces': [
+                { 'PrivateIpAddress': '172.26.35.126' },
+                { 'PrivateIpAddress': '172.26.101.56' },
+                { 'PrivateIpAddress': '172.26.38.168' }
+            ]
+        }
+        ec2_client.describe_network_interfaces = MagicMock(return_value=sample_response)
+
+        self.assertEqual(get_zookeeper_client(ec2_client), mock_kazzoo_client)
+
+        ec2_client.describe_network_interfaces.assert_called_with(Filters=[
+            {
+                'Name': 'tag:Component',
+                'Values': [
+                    'telemetry-zookeeper'
+                ]
+            }
+        ])
+        mock_kazzoo_client.start.assert_called_once()
+        mock_kazoo_constructor.assert_called_with(hosts=['172.26.35.126', '172.26.101.56', '172.26.38.168'])
+
+
+
+# class GetClickhouseClusterDefinition(unittest.TestCase):
+#
+#     def test_get_all_clickhouse_shards_to_instances(self):
+#         ec2_client = MagicMock()
+#         sample_response = {
+#             'NetworkInterfaces': [
+#                 { 'PrivateIpAddress': '172.26.35.126' },
+#                 { 'PrivateIpAddress': '172.26.101.56' },
+#                 { 'PrivateIpAddress': '172.26.38.168' }
+#             ]
+#         }
+#         ec2_client.describe_instances = MagicMock(return_value=sample_response)
+#
+#         ec2_client.describe_instances.assert_called_with(Filters=[
+#             {
+#                 'Name': 'tag-key',
+#                 'Values': [
+#                     'clickhouse-server'
+#                 ]
+#             }
+#         ])
+
+class LambdaHandler(unittest.TestCase):
+
+    @patch('clickhouse_config_in_zookeeper.boto3')
+    def test_get_all_clickhouse_instances(self, mock_boto3):
+        mock_client=MagicMock()
+        mock_boto3.client = MagicMock(return_value=mock_client)
+
+        lambda_handler({}, {})
+
+        mock_client.describe_instances.assert_called_with(Filters=[
+            {
+                'Name': 'tag-key',
+                'Values': [
+                    'clickhouse-server'
+                ]
+            }
+        ])
+
+    @patch('clickhouse_config_in_zookeeper.boto3')
+    def test_all_private_ips_and_shards_matching_clickhouse_server_added_to_zookeeper(self, mock_boto3):
+        mock_client=MagicMock()
+        mock_boto3.client = MagicMock(return_value=mock_client)
+
+        sampleResponse = {}
+        mock_client.describe_instances = MagicMock(return_value=sampleResponse)
+
